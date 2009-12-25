3042d8c0e08e1dc4dbff012768d95e61f978be35
Sam Soffes <sam@samsoff.es>
2009-12-25 02:22:59 -0500

    Example 1.5.4-1

---
+++ examples/1.5.4-1.c
@@ -0,0 +1,27 @@
+// Section 1.5.4 Example 1 from page 20
+
+#include <stdio.h>
+
+#define IN	1	/* inside a word */
+#define OUT	0	/* outside a word */
+
+/* count lines, words, and characters in input */
+main()
+{
+	int c, nl, nw, nc, state;
+	
+	state = OUT;
+	nl = nw = nc = 0;
+	while ((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if (state == OUT) {
+			state = IN;
+			++nw;
+		}
+	}
+	printf("%d %d %d\n", nl, nw, nc);
+}
