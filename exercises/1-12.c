c03e48e45b94f44647d9e48d193894fe67294c31
Sam Soffes <sam@samsoff.es>
2009-12-25 02:37:08 -0500

    Exercise 1-12

---
+++ exercises/1-12.c
@@ -0,0 +1,31 @@
+// Exercise 1-12 from page 21
+// Write a program that prints its input one word per line.
+
+#include <stdio.h>
+
+#define IN	1	/* inside a word */
+#define OUT	0	/* outside a word */
+
+main()
+{
+	int c, previous, state;
+	
+	state = OUT;
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+			if (previous == IN) {
+				putchar('\n');
+			}
+		}
+		else if (state == OUT) {
+			putchar(c);
+			state = IN;
+		}
+		else {
+			putchar(c);
+		}
+		previous = state;
+	}
+}
