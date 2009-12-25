d594db512d02d848f453f8cff94a8d09906f05f8
Sam Soffes <sam@samsoff.es>
2009-12-25 01:59:32 -0500

    Example 1.5.3-1

---
+++ examples/1.5.3-1.c
@@ -0,0 +1,15 @@
+// Section 1.5.3 Example 1 from page 19
+
+#include <stdio.h>
+
+/* count lines in input */
+main()
+{
+	int c, nl;
+	
+	nl = 0;
+	while ((c = getchar()) != EOF)
+		if (c == '\n')
+			++nl;
+	printf("%d\n", nl);
+}
