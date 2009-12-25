c50ee28bb36541f019522a6e8de715c78a3743d6
Sam Soffes <sam@samsoff.es>
2009-12-25 01:54:58 -0500

    Example 1.5.2-2

---
+++ examples/1.5.2-2.c
@@ -0,0 +1,14 @@
+// Section 1.5.2 Example 2 from page 18
+
+#include <stdio.h>
+
+/* count characters in input; 2nd version */
+main()
+{
+	double nc;
+	
+	nc = 0;
+	for (nc = 0; getchar() != EOF; ++nc)
+		;
+	printf("%.0f\n", nc);
+}
