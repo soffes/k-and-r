7cff0cc074ad138878a8fcbd65857b15cc3b15f7
Sam Soffes <sam@samsoff.es>
2009-12-25 01:16:19 -0500

    Example 1.3-1

---
+++ examples/1.3-1.c
@@ -0,0 +1,12 @@
+// Section 1.3 Example 1 from page 13
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table */
+main()
+{
+  int fahr;
+  
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
