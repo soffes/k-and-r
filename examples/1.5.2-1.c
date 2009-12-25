f3cde2c94d324aae11fbf5f1aa91239c291b29d0
Sam Soffes <sam@samsoff.es>
2009-12-25 01:51:29 -0500

    Example 1.5.2-1 (has a warning... boo)

---
+++ examples/1.5.2-1.c
@@ -0,0 +1,14 @@
+// Section 1.5.2 Example 1 from page 18
+
+#include <stdio.h>
+
+/* count characters in input; 1st version */
+main()
+{
+	long nc;
+	
+	nc = 0;
+	while (getchar() != EOF)
+		++nc;
+	printf("%1d\n", nc);
+}
